ab43a373107f33e3ba6f6d868ad16aa653492bad
timurIsakov <timur092000@gmail.com>
2024-09-20 00:15:10 +0500

    added get and save data to file json

--- main.py
+++ main.py
@@ -1,20 +1,41 @@
 import requests
+from bs4 import BeautifulSoup
+import json
 
 
 
-def get_urls(pages: int, path: str):
-    for page in range(1, pages + 1):
-        response = requests.get(f"{path}/interesting?page={page}")
-        print(type(response))
+def get_page(count_page: int, link: str)->str:
+    for page in range(1, count_page + 1):
+        response = requests.get(f"{link}/interesting?page={page}")
+        return response.text
 
 
+
+def get_data_from_page(page:str)-> list[dict]:
+    data = []
+    bs = BeautifulSoup(page, "html.parser")
+    titles_h5 = bs.find_all('h5', class_="title")
+    for title in titles_h5:
+        data.append({
+            "href": title.find("a")["href"],
+            "title": title.find("a")["title"]
+        })
+    return data
+
+
+
+def write_data_to_file(data:[dict]):
+    with open("data.json", 'w', encoding="utf-8", ) as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 
 def main():
     link = input("Enter link: ")
-    count_pages = int(input("Enter count pages: "))
-    get_urls(count_pages, link)
+    count_page = int(input("Enter count pages: "))
+    page = get_page(count_page, link)
+    write_data_to_file(get_data_from_page(page))
+
 
 
 
